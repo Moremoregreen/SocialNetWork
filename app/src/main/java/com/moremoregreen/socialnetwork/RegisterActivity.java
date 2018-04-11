7a47126ce44ecf6e23fd5a9b2740a06e87d90303
Moremoregreen <zzz2009j@gmail.com>
2018-04-12 02:14:22 +0800

    完成RegisterActivity findViews宣告

--- app/src/main/java/com/moremoregreen/socialnetwork/RegisterActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/RegisterActivity.java
@@ -2,12 +2,21 @@ package com.moremoregreen.socialnetwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText UserEmail, UserPassword, UserConfirmPassword;
+    private Button CreatAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        UserEmail = findViewById(R.id.register_email);
+        UserPassword = findViewById(R.id.register_password);
+        UserConfirmPassword = findViewById(R.id.register_confirm_password);
+        CreatAccountButton = findViewById(R.id.register_creat_account);
     }
 }
