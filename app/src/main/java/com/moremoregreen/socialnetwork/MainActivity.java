86c6ec5b270352a2c510ff83c4ac12001a0065c9
Moremoregreen <zzz2009j@gmail.com>
2018-04-11 15:11:47 +0800

    左側導覽layout初步建置完成

---
+++ app/src/main/java/com/moremoregreen/socialnetwork/MainActivity.java
@@ -0,0 +1,71 @@
+package com.moremoregreen.socialnetwork;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+//import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postlist;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);//有時候這邊會出錯appcompatActivity、v7相關的。到上面import V7Toolbar並刪掉沒V7的就好了
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_heder); //在navigation_menu上面放navigation_header
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()){
+//            case R.id.nav_post:
+//                Toast.makeText(this, "Post", Toast.LENGTH_SHORT).show();
+//                break;
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_friends:
+                Toast.makeText(this, "Friends", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_find_friends:
+                Toast.makeText(this, "Find_friends", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_message:
+                Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
