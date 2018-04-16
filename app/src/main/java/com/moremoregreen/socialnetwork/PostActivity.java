2bc01b67abee5cd09d018b9f79eeb56e756df477
Moremoregreen <zzz2009j@gmail.com>
2018-04-16 16:26:33 +0800

    發文頁面上面BAR新增

--- app/src/main/java/com/moremoregreen/socialnetwork/PostActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/PostActivity.java
@@ -1,13 +1,42 @@
 package com.moremoregreen.socialnetwork;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class PostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+
+        mToolbar = findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);//剛打完會error，要去import加上 .support.v7變成android.support.v7.widget.Toolbar;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //這邊是要弄個向左的箭頭(←)讓使用者可以視覺上可以點擊返回MainAct
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            SendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
