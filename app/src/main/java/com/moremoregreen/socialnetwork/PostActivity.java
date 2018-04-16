6e5cd090e974f078c47c6f951b26a3ecfb5602f6
Moremoregreen <zzz2009j@gmail.com>
2018-04-16 17:47:44 +0800

    選的圖顯示在imageButton上，上傳要Post的到Firebase

--- app/src/main/java/com/moremoregreen/socialnetwork/PostActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/PostActivity.java
@@ -1,14 +1,28 @@
 package com.moremoregreen.socialnetwork;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -18,11 +32,18 @@ public class PostActivity extends AppCompatActivity {
     private EditText PostDescription;
 
     private static final int Gallery_Pick = 1;
+    private Uri ImageUri;
+    private String Description;
+    private StorageReference PostImagesReference;
+
+    private String saveCurrentDate, saveCurrentTime, postRandomName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        PostImagesReference = FirebaseStorage.getInstance().getReference();
 
         SelectPostImage = findViewById(R.id.select_post_image);
         UpdatePostButton = findViewById(R.id.update_post_button);
@@ -42,6 +63,54 @@ public class PostActivity extends AppCompatActivity {
             }
         });
 
+        UpdatePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validatePostInfo();
+            }
+        });
+    }
+
+    private void validatePostInfo() {
+        Description = PostDescription.getText().toString();
+        if(ImageUri == null){
+            Toast.makeText(this, "Please Select Post Image...", Toast.LENGTH_SHORT).show();
+        }else if(TextUtils.isEmpty(Description)){
+            Toast.makeText(this, "Please Say Something About Your Image...", Toast.LENGTH_SHORT).show();
+        }else {
+            StoringImageToFirebaseStorage();
+
+        }
+    }
+
+    private void StoringImageToFirebaseStorage() {
+        //取得日期
+        Calendar calFordDate = Calendar.getInstance();
+        SimpleDateFormat  currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+        saveCurrentDate = currentDate.format(calFordDate.getTime());
+
+        //取得時間
+        Calendar calFordTime = Calendar.getInstance();
+        SimpleDateFormat  currentTime = new SimpleDateFormat("HH:mm");
+        saveCurrentTime = currentTime.format(calFordDate.getTime());
+
+        postRandomName = saveCurrentDate + saveCurrentTime;
+
+        StorageReference filePath = PostImagesReference
+                .child("Post Images")
+                .child(ImageUri.getLastPathSegment() + postRandomName + ".jpg");
+        filePath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(PostActivity.this, "Image Uploading Successfully to Storage...", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    String message = task.getException().getMessage();
+                    Toast.makeText(PostActivity.this, "Error Occured:" + message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void OpenGallery() {
@@ -49,6 +118,16 @@ public class PostActivity extends AppCompatActivity {
         galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
         galleryIntent.setType("image/*");
         startActivityForResult(galleryIntent, Gallery_Pick);
+    }
+
+    @Override
+    //讓選擇的圖片可以顯示在ImageButton上
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Gallery_Pick && resultCode == RESULT_OK && data!= null){
+            ImageUri = data.getData();
+            SelectPostImage.setImageURI(ImageUri);
+        }
     }
 
     @Override
