a97e36d8cba171952793bf84974d6ea695c163d6
Moremoregreen <zzz2009j@gmail.com>
2018-04-18 15:44:07 +0800

    實作只有PO文者可編輯刪除自己的貼文

--- app/src/main/java/com/moremoregreen/socialnetwork/ClickPostActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/ClickPostActivity.java
@@ -2,10 +2,12 @@ package com.moremoregreen.socialnetwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,13 +20,17 @@ public class ClickPostActivity extends AppCompatActivity {
     private TextView  PostDescription;
     private Button DeletePostButton, EditPostButton;
     private DatabaseReference ClickPostRef;
-    private String PostKey;
+    private String PostKey, currentUserID, databaseUserID, description, image;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_post);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid(); //取得是誰上線
 
         PostKey = getIntent().getExtras().get("PostKey").toString();
         ClickPostRef = FirebaseDatabase.getInstance().getReference().child("Posts").child(PostKey);
@@ -34,14 +40,25 @@ public class ClickPostActivity extends AppCompatActivity {
         DeletePostButton = findViewById(R.id.delete_post_button);
         EditPostButton = findViewById(R.id.edit_post_button);
 
+        //Button先隱藏起來
+        DeletePostButton.setVisibility(View.INVISIBLE);
+        EditPostButton.setVisibility(View.INVISIBLE);
+
         ClickPostRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String description = dataSnapshot.child("description").getValue().toString();
-                String image = dataSnapshot.child("postimage").getValue().toString();
+                description = dataSnapshot.child("description").getValue().toString();
+                image = dataSnapshot.child("postimage").getValue().toString();
+                databaseUserID = dataSnapshot.child("uid").getValue().toString();//取得PO文者的UID
 
                 PostDescription.setText(description);
                 Picasso.get().load(image).into(PostImage);
+
+                //如果上線的人跟PO文的人一樣的話，Button顯現出來
+                if(currentUserID.equals(databaseUserID)){
+                    DeletePostButton.setVisibility(View.VISIBLE);
+                    EditPostButton.setVisibility(View.VISIBLE);
+                }
             }
 
             @Override
