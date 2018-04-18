40e71cd979a671128846cf159ebfc9c6dcc954dd
Moremoregreen <zzz2009j@gmail.com>
2018-04-18 17:11:20 +0800

    編輯文章功能實作

--- app/src/main/java/com/moremoregreen/socialnetwork/ClickPostActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/ClickPostActivity.java
@@ -1,10 +1,14 @@
 package com.moremoregreen.socialnetwork;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -62,6 +66,14 @@ public class ClickPostActivity extends AppCompatActivity {
                         DeletePostButton.setVisibility(View.VISIBLE);
                         EditPostButton.setVisibility(View.VISIBLE);
                     }
+
+                    EditPostButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            EditCurrentPost(description);
+                        }
+                    });
+
                 }
             }
 
@@ -78,12 +90,33 @@ public class ClickPostActivity extends AppCompatActivity {
             }
         });
 
-        EditPostButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
+    }
+
+    private void EditCurrentPost(String description) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ClickPostActivity.this);
+        builder.setTitle("編輯貼文:");
+
+        final EditText inputField = new EditText(ClickPostActivity.this);
+        inputField.setText(description);
+        builder.setView(inputField);
+
+        builder.setPositiveButton("更新:", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ClickPostRef.child("description").setValue(inputField.getText().toString());
+                Toast.makeText(ClickPostActivity.this, "文章已成功編輯!", Toast.LENGTH_SHORT).show();
             }
         });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        Dialog dialog = builder.create();
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.holo_green_light);
     }
 
     private void DeleteCurrentPost() {
