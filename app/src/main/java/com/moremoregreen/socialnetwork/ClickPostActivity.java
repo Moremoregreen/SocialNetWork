b96a8b1c4868e11e779bd6fbfcb97480d1f06375
Moremoregreen <zzz2009j@gmail.com>
2018-04-18 14:55:36 +0800

    ClickPost.xml建置

---
+++ app/src/main/java/com/moremoregreen/socialnetwork/ClickPostActivity.java
@@ -0,0 +1,27 @@
+package com.moremoregreen.socialnetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ClickPostActivity extends AppCompatActivity {
+    private ImageView PostImage;
+    private TextView  PostDescription;
+    private Button DeletePostButton, EditPostButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_post);
+
+        PostImage = findViewById(R.id.post_image);
+        PostDescription = findViewById(R.id.post_description);
+        DeletePostButton = findViewById(R.id.delete_post_button);
+        EditPostButton = findViewById(R.id.edit_post_button);
+
+    }
+}
