c17a94189d306a1c3bfbc5b4a3ca9a53060d2d3e
Moremoregreen <zzz2009j@gmail.com>
2018-06-26 16:24:55 +0800

    忘記密碼寄送信件實作

--- app/src/main/java/com/moremoregreen/socialnetwork/ResetPasswordActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/ResetPasswordActivity.java
@@ -1,15 +1,26 @@
 package com.moremoregreen.socialnetwork;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private Button ResetPasswordSendEmailButton;
     private EditText ResetEmailInput;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +32,33 @@ public class ResetPasswordActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Reset Password");
 
+        mAuth = FirebaseAuth.getInstance();
+
         ResetPasswordSendEmailButton = findViewById(R.id.reset_password_email_button);
         ResetEmailInput = findViewById(R.id.reset_password_EMAIL);
+
+        ResetPasswordSendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = ResetEmailInput.getText().toString();
+                if(TextUtils.isEmpty(userEmail)){
+                    Toast.makeText(ResetPasswordActivity.this, "請輸入信箱...", Toast.LENGTH_SHORT).show();
+                }else {
+                    mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(ResetPasswordActivity.this, "信件已寄出，請檢查信箱。", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                            }else {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(ResetPasswordActivity.this, "發生錯誤:" + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
     }
 }
