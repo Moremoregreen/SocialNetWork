d0f2051694889501f5a0108bd69ea3cfc66f76c9
Moremoregreen <zzz2009j@gmail.com>
2018-06-28 02:54:25 +0800

    好友邀請發送與取消實作

--- app/src/main/java/com/moremoregreen/socialnetwork/PersonProfileActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/PersonProfileActivity.java
@@ -1,10 +1,15 @@
 package com.moremoregreen.socialnetwork;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,19 +25,19 @@ public class PersonProfileActivity extends AppCompatActivity {
     private CircleImageView userProfileImage;
     private Button SendFriendRequestButton , DeclineFriendRequestButton;
 
-    private DatabaseReference profileUserRef, UsersRef;
+    private DatabaseReference FriendRequestRef, UsersRef;
     private FirebaseAuth mAuth;
-    private String senderUserId , receiverUserId;
+    private String senderUserId , receiverUserId , CURRENT_STATE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_profile);
 
         mAuth = FirebaseAuth.getInstance();
-
+        senderUserId = mAuth.getCurrentUser().getUid();
         receiverUserId = getIntent().getExtras().get("visit_user_id").toString();
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
-
+        FriendRequestRef = FirebaseDatabase.getInstance().getReference().child("FriendRequests");
         IntializeFields();
 
         UsersRef.child(receiverUserId).addValueEventListener(new ValueEventListener() {
@@ -59,6 +64,8 @@ public class PersonProfileActivity extends AppCompatActivity {
                     userCountry.setText("國家：" + myCountry);
                     userGender.setText("性別：" + myGender);
                     userRelation.setText("社交關係：" + myRelationStatus);
+
+                    MaintananceofButtons();
                 }
             }
 
@@ -67,6 +74,101 @@ public class PersonProfileActivity extends AppCompatActivity {
 
             }
         });
+
+        DeclineFriendRequestButton.setVisibility(View.INVISIBLE);
+        DeclineFriendRequestButton.setEnabled(false);
+
+        if(!senderUserId.equals(receiverUserId)){
+            SendFriendRequestButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SendFriendRequestButton.setEnabled(false);
+                    Toast.makeText(PersonProfileActivity.this, "請求已送出", Toast.LENGTH_SHORT).show();
+
+                    if(CURRENT_STATE.equals("not_friends")) {
+                        SendFriendRequestToaPerson();
+                    }
+                    if(CURRENT_STATE.equals("request_sent")) {
+                        CancelFriendRequest();
+                    }
+                }
+            });
+        }else {
+            DeclineFriendRequestButton.setVisibility(View.INVISIBLE);  //自己不能加自己好友，所以隱藏button
+            SendFriendRequestButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void CancelFriendRequest() {
+        FriendRequestRef.child(senderUserId).child(receiverUserId)
+                .removeValue()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            FriendRequestRef.child(receiverUserId).child(senderUserId)
+                                    .removeValue()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            SendFriendRequestButton.setEnabled(true);
+                                            CURRENT_STATE = "not_friends";
+                                            SendFriendRequestButton.setText("發送好友邀請");
+                                            DeclineFriendRequestButton.setVisibility(View.INVISIBLE);
+                                            DeclineFriendRequestButton.setEnabled(false);
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+
+    private void MaintananceofButtons() {
+        FriendRequestRef.child(senderUserId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(receiverUserId)){
+                            String request_type = dataSnapshot.child(receiverUserId).child("request_type")
+                                    .getValue().toString();
+                            if(request_type.equals("sent")){
+                                CURRENT_STATE = "request_sent";
+                                SendFriendRequestButton.setText("取消好友邀請");
+                                DeclineFriendRequestButton.setVisibility(View.INVISIBLE);
+                                DeclineFriendRequestButton.setEnabled(false);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void SendFriendRequestToaPerson() {
+        FriendRequestRef.child(senderUserId).child(receiverUserId)
+                .child("request_type").setValue("sent")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            FriendRequestRef.child(receiverUserId).child(senderUserId)
+                                    .child("request_type").setValue("received")
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            SendFriendRequestButton.setEnabled(true);
+                                            CURRENT_STATE = "request_sent";
+                                            SendFriendRequestButton.setText("取消好友邀請");
+                                            DeclineFriendRequestButton.setVisibility(View.INVISIBLE);
+                                            DeclineFriendRequestButton.setEnabled(false);
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 
     private void IntializeFields() {
@@ -80,6 +182,8 @@ public class PersonProfileActivity extends AppCompatActivity {
         userProfileImage = findViewById(R.id.person_profile_pic);
         SendFriendRequestButton = findViewById(R.id.person_send_friend_requset_button);
         DeclineFriendRequestButton = findViewById(R.id.person_decline_friend_requset);
+
+        CURRENT_STATE = "not_friends";
 
     }
 }
