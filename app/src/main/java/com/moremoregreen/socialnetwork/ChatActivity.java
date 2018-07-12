12c8c2025428652f065b274b01ebc7e9dc7dbf4a
Moremoregreen <zzz2009j@gmail.com>
2018-07-12 18:57:38 +0800

    Chat.xml 上面Bar讀取UserName跟ProfileImage

--- app/src/main/java/com/moremoregreen/socialnetwork/ChatActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/ChatActivity.java
@@ -1,17 +1,35 @@
 package com.moremoregreen.socialnetwork;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
     private Toolbar ChattoolBar;
     private ImageButton SendMessageButton, SendImgagefileButton;
     private EditText userMessageInput;
     private RecyclerView userMessageList;
+    private TextView receiverName;
+    private CircleImageView receiverProfileImage;
+    private DatabaseReference RootRef;
 
     private String messageReceiverID, messageReceiverName;
 
@@ -20,17 +38,46 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        RootRef = FirebaseDatabase.getInstance().getReference();
         messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
         messageReceiverName = getIntent().getExtras().get("userName").toString();
 
         IntializeFields();
+        DiscplayReceiverInfo();
+    }
 
+    private void DiscplayReceiverInfo() {
+        receiverName.setText(messageReceiverName);
+        RootRef.child("Users").child(messageReceiverID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    final String profileImage = dataSnapshot.child("profileimage").getValue().toString();
+                    Picasso.get().load(profileImage).placeholder(R.drawable.profile).into(receiverProfileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void IntializeFields() {
         ChattoolBar = findViewById(R.id.chat_bar_layout);
         setSupportActionBar(ChattoolBar);
 
+        //把chat_custom_bar加進來
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View action_bar_view = layoutInflater.inflate(R.layout.chat_custom_bar, null);
+        actionBar.setCustomView(action_bar_view);
+
+        receiverName = findViewById(R.id.custom_profile_name);
+        receiverProfileImage = findViewById(R.id.custom_profile_image);
         SendMessageButton = findViewById(R.id.send_message_button);
         SendImgagefileButton = findViewById(R.id.send_image_file_button);
         userMessageInput = findViewById(R.id.input_message);
