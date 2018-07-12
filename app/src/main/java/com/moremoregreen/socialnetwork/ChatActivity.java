cf34f1797089e495469eaf96dd36edd3567aaf89
Moremoregreen <zzz2009j@gmail.com>
2018-07-12 23:40:37 +0800

    傳送聊天訊息與接收實作

--- app/src/main/java/com/moremoregreen/socialnetwork/ChatActivity.java
+++ app/src/main/java/com/moremoregreen/socialnetwork/ChatActivity.java
@@ -1,24 +1,37 @@
 package com.moremoregreen.socialnetwork;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,7 +44,8 @@ public class ChatActivity extends AppCompatActivity {
     private CircleImageView receiverProfileImage;
     private DatabaseReference RootRef;
 
-    private String messageReceiverID, messageReceiverName;
+    private String messageReceiverID, messageReceiverName, messageSenderID, saveCurrentDate, saveCurrentTime;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +56,70 @@ public class ChatActivity extends AppCompatActivity {
         messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
         messageReceiverName = getIntent().getExtras().get("userName").toString();
 
+        mAuth = FirebaseAuth.getInstance();
+        messageSenderID = mAuth.getCurrentUser().getUid();
+
         IntializeFields();
         DiscplayReceiverInfo();
+        SendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendMessage();
+            }
+        });
+        SendImgagefileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void SendMessage() {
+        String messageText = userMessageInput.getText().toString();
+        if (TextUtils.isEmpty(messageText)) {
+            Toast.makeText(this, "請輸入訊息", Toast.LENGTH_SHORT).show();
+        } else {
+            String message_sender_ref = "Messages/" + messageSenderID + "/" + messageReceiverID;
+            String message_recevier_ref = "Messages/" + messageReceiverID + "/" + messageSenderID;
+            DatabaseReference user_message_key = RootRef.child("Messages")
+                    .child(messageSenderID).child(messageReceiverID).push();
+            String message_push_id = user_message_key.getKey();
+
+            //取得日期
+            Calendar calFordDate = Calendar.getInstance();
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+            saveCurrentDate = currentDate.format(calFordDate.getTime());
+
+            //取得時間
+            Calendar calFordTime = Calendar.getInstance();
+            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm aa");
+            saveCurrentTime = currentTime.format(calFordDate.getTime());
+
+            Map messageTextBody = new HashMap();
+            messageTextBody.put("message", messageText);
+            messageTextBody.put("time", saveCurrentTime);
+            messageTextBody.put("date", saveCurrentDate);
+            messageTextBody.put("type", "text");
+            messageTextBody.put("from", messageSenderID);
+
+            Map messageBodyDetails = new HashMap();
+            messageBodyDetails.put(message_sender_ref + "/" + message_push_id , messageTextBody );
+            messageBodyDetails.put(message_recevier_ref + "/" + message_push_id , messageTextBody );
+
+            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(ChatActivity.this, "訊息已送出", Toast.LENGTH_SHORT).show();
+                    }else {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(ChatActivity.this, "發生錯誤: " + message , Toast.LENGTH_SHORT).show();
+                    }
+                    userMessageInput.setText("");
+                }
+            });
+        }
     }
 
     private void DiscplayReceiverInfo() {
@@ -51,7 +127,7 @@ public class ChatActivity extends AppCompatActivity {
         RootRef.child("Users").child(messageReceiverID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     final String profileImage = dataSnapshot.child("profileimage").getValue().toString();
                     Picasso.get().load(profileImage).placeholder(R.drawable.profile).into(receiverProfileImage);
                 }
